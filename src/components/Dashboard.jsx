a1cdb02e7b1bcfe5de37deed8a11614004711012
Shiyas ps <shiyasofficial1@gmail.com>
2025-10-05 12:48:44 +0530

    mistakes

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,15 +3,15 @@ function Dashboard() {
         <section className="m-3">
             <div className="flex justify-around item-center p-10 border border-gray-700 bg-gradient-to-br from-gray-800 to-gray-900 rounded-xl">
                 <div className="flex flex-col gap-1 justify-center items-center">
-                    <img className="h-13" src="/public/icon=tracked-tasks.png" alt="" />
+                    <img className="h-13" src="/images/tracked-tasks.png" alt="" />
                     <p className="text-5xl">500</p>
                 </div>
                 <div className="flex flex-col gap-1 justify-center items-center">
-                    <img className="h-13" src="/public/icon=completed.png" alt="" />
+                    <img className="h-13" src="/images/completed.png" alt="" />
                     <p className="text-5xl">500</p>
                 </div>
                 <div className="flex flex-col gap-1 justify-center items-center">
-                    <img className="h-13" src="/public/icon=karma-points.png" alt="" />
+                    <img className="h-13" src="/images/karma-points.png" alt="" />
                     <p className="text-5xl">500</p>
                 </div>
             </div>
