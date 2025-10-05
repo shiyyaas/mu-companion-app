dc20877b58a7a1ba7ad8e4865fad40db0571b8b2
Shiyas ps <shiyasofficial1@gmail.com>
2025-10-05 11:32:51 +0530

    Category nice

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,8 +1,11 @@
 function Category() {
     return (
         <nav className="">
-            <div className="">
-                Category
+            <div className="flex justify-center ">
+                <p className="pl-5 p-3 border border-slate-700 rounded-l-3xl hover:bg-gray-700">All Tasks</p>
+                <p className="p-3 border border-slate-700 hover:bg-gray-700">Ongoing</p>
+                <p className="p-3 border border-slate-700 hover:bg-gray-700">Starred</p>
+                <p className="pr-5 p-3 border border-slate-700 rounded-r-3xl hover:bg-gray-700">Completed</p>
             </div>
         </nav>
     );
