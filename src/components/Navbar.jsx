128c73ea8288adc2ea10e26f64f40d6c159efa98
Shiyas ps <shiyasofficial1@gmail.com>
2025-10-05 10:11:08 +0530

    Pushing to deploy

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+function Navbar() {
+    return (
+        <nav>
+            <div className="flex">
+                
+                {/* <img src="https://mucompanion.vercel.app/images/shiyas.jpeg" alt="" /> */}
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar
