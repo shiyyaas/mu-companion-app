cf87e86924f22efc50b06b86842347d20d800f49
Shiyas ps <shiyasofficial1@gmail.com>
2025-10-06 20:03:44 +0530

    Some changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
         <nav>
             <div className="flex items-center py-3 px-5 border-b border-gray-500 mt-3 mx-3">
                 <a href="#"><img className="h-12" src="/images/menu.png" alt="" /></a>
-                <a className="ml-auto font-mono text-3xl " href="">mu-companion</a>
+                <a className="ml-auto font-sans font-thin text-3xl" href="">mu-companion</a>
             </div>
         </nav>
     );
